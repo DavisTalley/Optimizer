8012b45a1b2b56d1662fcb8e2c44362b52cfe8cb
Davis Talley <dtalley@bloxbuilt.com>
2017-08-10 14:44:07 -0500

    ..

--- optimize1.py
+++ optimize1.py
@@ -18,7 +18,7 @@ skills = ["Frame", "Weld", "Plumb", "Wire", "Squanch"]
 products, revenue = multidict({"BR" : 10, "HW" : 7, "FW" : 6, 
                                "SW" : 5, "CS" : 3, "MEP" : 14, "UBER": 100})
 
-line, mode = multidict({"BR1" : {'BR1.0', 'BR1.1', 'BR1.2', 'BR1.3'},
+lines, mode = multidict({"BR1" : {'BR1.0', 'BR1.1', 'BR1.2', 'BR1.3'},
                         "BR2" : {'BR2.0', 'BR2.1', 'BR2.2', 'BR2.3'},
                         "HW1" : {'HW1.0', 'HW1.1', 'HW1.2', 'HW1.3'},
                         "HW2" : {'HW2.0', 'HW2.1', 'HW2.2', 'HW2.3'},
@@ -29,9 +29,7 @@ line, mode = multidict({"BR1" : {'BR1.0', 'BR1.1', 'BR1.2', 'BR1.3'},
                         "UBER1" : {'UBER1.0', 'UBER1.0.1', 'UBER1.0.2', 'UBER1.0.25'},
                         "UBER2" : {'UBER2.0', 'UBER2.0.1', 'UBER2.0.2', 'UBER2.0.25'}
                        })
-
-
-
+ 
 workers_skills_table = {
     "Rick" : {"Frame": 1, "Plumb": 1},
     "Morty" : {"Weld": 1},
@@ -45,6 +43,6 @@ workers_skills_table = {
     "Sleepy Gary" : {"Plumb": 1}
 }
 
-prod_demand = {("Fri1", "BR") : 4, ("Fr1", "HW") : 8, ("Fri1", "MEP") : 2}
+production_demand = {("Fri1", "BR") : 4, ("Fr1", "HW") : 8, ("Fri1", "MEP") : 2}
 
 print(skills)
